3012695895cce030a7de248aed96bdf10b235d76
Arahk Yambupah <arahk@outlook.com>
2024-02-06 22:03:33 +0700

    Read Excel file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include "Services/ExcelService.h"
 #include "libs/OpenXLSX/OpenXLSX.hpp"
 
@@ -6,14 +7,31 @@ using namespace std;
 using namespace Services;
 using namespace OpenXLSX;
 
-int main() {   
+void ReadExcelFile(string excelFile)
+{
+    XLDocument doc;
+    doc.open(excelFile);
 
-    cout << "Hello World" << endl;
+    auto wks = doc.workbook().worksheet("DisputeATM");
 
-    string name = Services::GetName();
+    int totalRows = wks.rowCount();
 
-    cout << "You name is " + name << endl;
+    for (int i = 2; i < totalRows; i++)
+    {
+        XLCellValue createDateCellValue = wks.cell("B" + to_string(i)).value();
+        XLCellValue termIdCellValue = wks.cell("H" + to_string(i)).value();
+        XLCellValue branchCellValue = wks.cell("I" + to_string(i)).value();
 
+        cout << "RowNo: " + to_string(i) 
+             << ", Create Date: " + createDateCellValue.get<string>() 
+             << ", Term Id: " + termIdCellValue.get<string>() 
+             << ", Branch: " + branchCellValue.get<string>() 
+             << endl;
+    }
+}
+
+void CreateExcelFile()
+{
     XLDocument doc;
     doc.create("Spreadsheet.xlsx");
 
@@ -22,6 +40,20 @@ int main() {
     wks.cell("A1").value() = "Hello, OpenXLSX!";
 
     doc.save();
+}
+
+int main()
+{
+
+    cout << "Hello World" << endl;
+
+    string name = Services::GetName();
+
+    cout << "You name is " + name << endl;
+
+    string excelFile = "/Users/arahkyambubpah/Sources/Playgrounds/C++/ExcelReader/files/DisputeATM_01-260124.xlsx";
+
+    ReadExcelFile(excelFile);
 
     return 0;
 }
